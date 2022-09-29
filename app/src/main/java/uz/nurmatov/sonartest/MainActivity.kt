9527f842755927fd75d1a487fa1b53bc41dae851
BaxtiyorNurmatov <baxtiyor@nurmatov.uz>
2022-09-29 20:41:55 +0500

    Initialized a new SonarTest project

---
+++ app/src/main/java/uz/nurmatov/sonartest/MainActivity.kt
@@ -0,0 +1,40 @@
+package uz.nurmatov.sonartest
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import uz.nurmatov.sonartest.ui.theme.SonarTestTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            SonarTestTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    SonarTestTheme {
+        Greeting("Android")
+    }
+}
